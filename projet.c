4a25761bd8a5fa9a0f012433990893ab3d71a2fd
Vvalfer <louischevalier833@gmail.com>
2025-10-10 11:49:04 +0200

    debut d'ajout de fonction pour le test (conversion en decimal), ajout de la soustraction (a bosser encore), ajout de fonction pour creer grand binaire depuis chaine de char

--- projet.c
+++ projet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define BASE 2
 
 // Creation d'une structure pour la..
@@ -24,6 +26,35 @@ BigBinary initBigBinary (int taille, int signe) {
     return nb;
 }
 
+// creation d'un nombre binaire de grande taille a partir d'une chaine
+BigBinary createBigBinaryFromStr(const char *str) {
+    BigBinary nb;
+    int n = strlen(str);
+    nb.Taille = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (str[i] == '0' || str[i] == '1') {
+            nb.Taille++;
+        }
+    }
+
+    nb.Tdigits = malloc(sizeof(int) * nb.Taille);
+    nb.Signe = +1;
+    int index = 0;
+    int allZero = 1;
+
+    for (int i = 0; i < n; ++i) {
+        if (str[i] == '0' || str[i] == '1') {
+            nb.Tdigits[index] = str[i] - '0';
+            if (nb.Tdigits[index] == 1) allZero = 0;
+            index++;
+        }
+    }
+
+    if (allZero) nb.Signe = 0;
+    return nb;
+}
+
 // Affichage d'un nombre binaire de grande taille
 void afficheBigBinary (BigBinary nb) {
     if (nb.Signe == -1) printf("-");
@@ -37,7 +68,7 @@ void afficheBigBinary (BigBinary nb) {
     printf("\n");
 }
 
-// Liberation de la memoire allouee dynamiquement pour un nombre binaire de grande taille
+// Liberation de la memoire allouee dynamiquement
 void libereBigBinary(BigBinary *nb){
     free(nb->Tdigits);
     nb->Tdigits = NULL;
@@ -46,23 +77,78 @@ void libereBigBinary(BigBinary *nb){
 }
 
 void divisePar2(BigBinary *nb) {
+    // fonction d'exemple a faire plus tard (pas mandatory)
+}
+
+void egale(BigBinary *a, BigBinary *b) {
+    // thomas a faire
+}
+
+void inferieur(BigBinary *a, BigBinary *b) {
+    // louis a faire
+}
+
+int soustraction(BigBinary *a, BigBinary *b, BigBinary *res) {
+    if ((a -> Taille >= b -> Taille)) { // si memes tailles et memes signe
+        int taille_max = a -> Taille;
+        // travailler sur allocation dynamique
+        for (int i = 0; i < taille_max; ++i) { // boucle de soustraction sur chaque element du tableau
+            res -> Tdigits[i] = a -> Tdigits[i] - b -> Tdigits[i]; // soustraction element par element
+            if (res -> Tdigits[i] < 0) { // si negatif, on emprunte
+                res -> Tdigits[i] += BASE; // on ajoute la base (2)
+                res -> Tdigits[i - 1] -= 1; // on soustrait 1 a l'element precedent
+            }
+            res -> Taille = taille_max;
+        }
+        return 0; // succes
+    } else {
+        return -1; // erreur, a < b
+    }
+}
+
+// fonction d'addition (thomas)
+
+int convertirEnDecimal(BigBinary nb) { // a degager apres test
+    int decimal = 0;
+    for (int i = 0; i < nb.Taille; ++i) {
+        decimal = decimal * BASE + nb.Tdigits[i];
+    }
+    return decimal;
 }
 
 int main() {
-    int bits83[] = {1, 0, 1, 0, 0, 1, 1};
+    int bits83[] = {1, 0, 1, 1, 0, 1};
+    int bit40[] = {1, 1, 1, 1};
     int taille = sizeof(bits83) / sizeof(bits83[0]);
+    int taille2 = sizeof(bit40) / sizeof(bit40[0]);
 
     BigBinary nb = initBigBinary(taille, +1);
-    for (int i = 0, i < taille; ++i) {
+    BigBinary nb2 = initBigBinary(taille2, +1);
+    BigBinary res;
+    for (int i = 0; i < taille; ++i) {
         nb.Tdigits[i] =  bits83[i];
     }
+    for (int i = 0; i < taille2; ++i) {
+        nb2.Tdigits[i] =  bit40[i];
+    }
 
-    printf("Valeur initiale : ");
+    printf("Valeur 1 initiale : ");
     afficheBigBinary(nb);
+    printf("taille : %d\n", nb.Taille);
+    printf("Valeur 2 initiale : ");
+    printf("taille2 : %d\n", nb2.Taille);
+    afficheBigBinary(nb2);
+    printf("Valeur en decimal1 : %d\n", convertirEnDecimal(nb));
+    printf("Valeur en decimal2 : %d\n", convertirEnDecimal(nb2));
 
-    // faire addition puis soustraction et afficher resultat
+    soustraction(&nb, &nb2, &res);
+    printf("Valeur apres soustraction : ");
+    afficheBigBinary(res);
+    printf("Valeur en decimal : %d\n", convertirEnDecimal(res));
 
 
     libereBigBinary(&nb);
+    libereBigBinary(&nb2);
+    libereBigBinary(&res);
     return 0;
 }
